ecf4a60488bed4755f1563281a54c23c75af0255
Nirajan Dhakal <sorcerersupreme010@gmail.com>
2026-03-10 16:51:54 +0545

    fixing bugs for production

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,15 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import { resolve } from 'path'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { fileURLToPath, URL } from "node:url"; // Modern way to handle paths in ESM
 
 export default defineConfig({
+  base: "/wikiroulette/",
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': resolve(__dirname, 'src'),
+      // This achieves the same thing as resolve(__dirname, "src")
+      // but is more compatible with modern ES Modules
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
-})
+});
